8f610b9c19e10419121b6b0e3e7ee613d5b23abc
MarbhDamhsa <jmendelsohn11@bristolcc.edu>
2016-12-28 19:46:36 -0500

    Update rps.hs

--- rps.hs
+++ rps.hs
@@ -1,7 +1,9 @@
+--Create custom data type for Rock, Paper, and Scissors
 data RPS = Rock 
          | Paper 
          | Scissors
-
+         
+--Takes two of RPS and returns a string
 shoot :: RPS -> RPS -> String
 shoot Paper Rock = "Paper Beats Rock"
 shoot Paper Scissors = "Paper Loses to Scissors"
@@ -13,3 +15,5 @@ shoot Scissors Paper = "Scissors Beats Paper"
 shoot Scissors Rock = "Scissors Loses to Rock"
 shoot Scissors Scissors = "Tie"
 shoot _ _ = "Error"
+
+{- To implement: AI -}
